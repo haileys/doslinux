1c6a591c33844cf1b9bee36bfe79ef2d4a7a7c40
Hailey Somerville <hailey@hailey.lol>
2020-09-26 15:05:20 +1000

    move types to vm86 header

--- init/vm86.c
+++ init/vm86.c
@@ -15,50 +15,6 @@
 #include "term.h"
 #include "vm86.h"
 
-#define DOSLINUX_INT 0xe7
-
-typedef union reg32 {
-    uint32_t dword;
-    struct {
-        uint16_t lo;
-        uint16_t hi;
-    } word;
-    struct {
-        uint8_t lo;
-        uint8_t hi;
-        uint8_t res1;
-        uint8_t res2;
-    } byte;
-}
-reg32_t;
-
-// this is like vm86_regs, but all registers are of union type reg32_t
-typedef struct regs {
-    reg32_t ebx;
-    reg32_t ecx;
-    reg32_t edx;
-    reg32_t esi;
-    reg32_t edi;
-    reg32_t ebp;
-    reg32_t eax;
-    reg32_t __null_ds;
-    reg32_t __null_es;
-    reg32_t __null_fs;
-    reg32_t __null_gs;
-    reg32_t orig_eax;
-    reg32_t eip;
-    reg32_t cs;
-    reg32_t eflags;
-    reg32_t esp;
-    reg32_t ss;
-
-    reg32_t es16;
-    reg32_t ds16;
-    reg32_t fs16;
-    reg32_t gs16;
-}
-regs_t;
-
 typedef struct task {
     regs_t* regs;
     kbd_t kbd;
